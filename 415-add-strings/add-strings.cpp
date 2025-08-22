fc952b7b4af42fb2e2ef0a9b85bd0c5ef21cf3e5
Unnati Mittal <153089498+Unnati147@users.noreply.github.com>
2025-08-22 12:20:50 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (42.26%) - LeetSync

--- 415-add-strings/add-strings.cpp
+++ 415-add-strings/add-strings.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-string addRE(string num1, string num2, int p1, int p2, int carry){
+void addRE(string&num1, string&num2, int p1, int p2, int carry, string&ans){
     // base case
     if(p1 < 0 && p2 < 0){
         if(carry != 0){
-            return string(1, carry +'0');
+            ans.push_back(carry +'0');
         }
-        return "";
+        return;
     }
 
     // ek case jo hum krenge
@@ -15,17 +15,16 @@ string addRE(string num1, string num2, int p1, int p2, int carry){
     int csum = n1+n2+carry;
     int digit = csum % 10;
     carry = csum /10;
-    string ans= "";
     ans.push_back(digit +'0');
 
     // baki recursion krega
 
-    ans += addRE(num1,  num2, p1-1, p2-1, carry);
-    return ans;
-
+    addRE(num1,  num2, p1-1, p2-1, carry,ans);
+    
 }
     string addStrings(string num1, string num2) {
-        string ans= addRE(num1,num2, num1.size()-1, num2.size()-1, 0);
+        string ans="";
+        addRE(num1,num2, num1.size()-1, num2.size()-1, 0,ans);
         reverse(ans.begin(), ans.end());
         return ans;
     }
