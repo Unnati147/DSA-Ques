51ed6beedadee4d38cfdec6ebcb1722f611fe6fa
Unnati Mittal <153089498+Unnati147@users.noreply.github.com>
2025-09-11 16:33:38 +0530

    Time: 63 ms (66.51%) | Memory: 73 MB (68.99%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,62 +1,85 @@
 class Solution {
 public:
-    void merge(vector<int>& arr, int s, int e) {
+    void merge(vector<int>& arr, vector<int>& temp, int s, int e) {
         int mid = (s + e) / 2;
-        int leftArray = mid - s + 1;
-        int rightArray = e - mid;
+        // int leftArray = mid - s + 1;
+        // int rightArray = e - mid;
 
-        // create left and right dynamic arrays
-        int* left = new int[leftArray];
-        int* right = new int[rightArray];
+        // // create left and right dynamic arrays
+        // int* left = new int[leftArray];
+        // int* right = new int[rightArray];
 
-        // copy values into left array
-        int k = s;
-        for (int i = 0; i < leftArray; i++) {
-            left[i] = arr[k];
-            k++;
+        // // copy values into left array
+        // int k = s;
+        // for (int i = 0; i < leftArray; i++) {
+        //     left[i] = arr[k];
+        //     k++;
+        // }
+
+        // // copy values into right array
+        // k = mid + 1;
+        // for (int i = 0; i < rightArray; i++) {
+        //     right[i] = arr[k];
+        //     k++;
+        // }
+
+        // // merge logic
+        // int leftIndex = 0, rightIndex = 0, mainIndex = s;
+
+        // while (leftIndex < leftArray && rightIndex < rightArray) {
+        //     if (left[leftIndex] < right[rightIndex]) {
+        //         arr[mainIndex++] = left[leftIndex++];
+        //     } else {
+        //         arr[mainIndex++] = right[rightIndex++];
+        //     }
+        // }
+
+        // // remaining elements
+        // while (leftIndex < leftArray) {
+        //     arr[mainIndex++] = left[leftIndex++];
+        // }
+        // while (rightIndex < rightArray) {
+        //     arr[mainIndex++] = right[rightIndex++];
+        // }
+
+        // // free memory
+        // delete[] left;
+        // delete[] right;
+
+        int k =s;
+        int i = s, j = mid+1;
+        while(i  <= mid && j <= e){
+            if(arr[i]<=arr[j])
+            temp[k++] = arr[i++];
+            else
+            temp[k++] = arr[j++];
         }
 
-        // copy values into right array
-        k = mid + 1;
-        for (int i = 0; i < rightArray; i++) {
-            right[i] = arr[k];
-            k++;
+        while(i <= mid)
+        temp[k++] = arr[i++];
+
+        while(j <= e){
+            temp[k++] = arr[j++];
         }
 
-        // merge logic
-        int leftIndex = 0, rightIndex = 0, mainIndex = s;
+        while(s<=e){
+            arr[s] = temp[s];
+            s++;
 
-        while (leftIndex < leftArray && rightIndex < rightArray) {
-            if (left[leftIndex] < right[rightIndex]) {
-                arr[mainIndex++] = left[leftIndex++];
-            } else {
-                arr[mainIndex++] = right[rightIndex++];
-            }
         }
-
-        // remaining elements
-        while (leftIndex < leftArray) {
-            arr[mainIndex++] = left[leftIndex++];
-        }
-        while (rightIndex < rightArray) {
-            arr[mainIndex++] = right[rightIndex++];
-        }
-
-        // free memory
-        delete[] left;
-        delete[] right;
     }
 
-    void mergeSort(vector<int>& arr, int s, int e) {
+    void mergeSort(vector<int>& arr, vector<int> &temp, int s, int e) {
         if (s >= e) return;
         int mid = (s + e) / 2;
-        mergeSort(arr, s, mid);
-        mergeSort(arr, mid + 1, e);
-        merge(arr, s, e);
+        mergeSort(arr,temp, s, mid);
+        mergeSort(arr,temp, mid + 1, e);
+        merge(arr,temp, s, e);
     }
 
     vector<int> sortArray(vector<int>& nums) {
-        mergeSort(nums, 0, nums.size() - 1);
+        vector<int> temp (nums.size(),0);
+        mergeSort(nums,temp,  0, nums.size() - 1);
         return nums;
     }
 };
