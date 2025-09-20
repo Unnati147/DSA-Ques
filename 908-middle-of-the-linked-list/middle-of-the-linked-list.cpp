9c818807280dacf7e99f39833c215ad9ce65755e
Unnati Mittal <153089498+Unnati147@users.noreply.github.com>
2025-09-20 17:28:47 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (25.57%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -19,18 +19,32 @@ int getLength(ListNode* head){
     }
     return len;
 }
-    ListNode* middleNode(ListNode* head) {
+    // ListNode* middleNode(ListNode* head) {
+    //     int n= getLength(head);
+    //     int position = n/2 + 1;
+    //     int currPos = 1;
+    //     ListNode* temp = head;
+    //     while(currPos != position){
+      
+    //         currPos++;
+    //         temp = temp->next;
+    //     }
+    //     return temp;
+    // }
+
+     ListNode* middleNode(ListNode* head) {
         int n= getLength(head);
         int position = n/2 + 1;
-        int currPos = 1;
         ListNode* temp = head;
-        while(currPos != position){
-      
-            currPos++;
-            temp = temp->next;
+
+        while(position  != 1){
+            position --;
+            temp = temp ->next;
+
         }
         return temp;
     }
+
 
     //     ListNode* fast= head;
     //     ListNode* slow = head;
