f8711efefc39330847e496235b7ffed22ac8e9cf
Unnati Mittal <153089498+Unnati147@users.noreply.github.com>
2025-09-21 12:48:30 +0530

    Time: 4 ms (96.87%) | Memory: 11.8 MB (80.03%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -8,21 +8,46 @@
  */
 class Solution {
 public:
-    bool hasCycle(ListNode *head) {
-  
 
-    map<ListNode*, bool> table;
-    ListNode* temp = head;
-    while(temp != NULL){
-        if(table[temp] == false){
-            table[temp] = true;
-        }else{
+bool hasCycle(ListNode *head) {
+    ListNode* slow= head;
+    ListNode* fast= head;
+    while(fast != NULL){
+        fast = fast->next;
+        if(fast != NULL){
+            fast=fast->next;
+            slow= slow->next;
+            if(slow == fast){
             return true;
         }
-        temp = temp ->next;
+        }
     }
     return false;
 
+}
 
-    }
+
+
+
+
+
+
+
+
+    // bool hasCycle(ListNode *head) {
+  
+// map wali approach
+
+    // map<ListNode*, bool> table;
+    // ListNode* temp = head;
+    // while(temp != NULL){
+    //     if(table[temp] == false){
+    //         table[temp] = true;
+    //     }else{
+    //         return true;
+    //     }
+    //     temp = temp ->next;
+    // }
+    // return false;
+// }
 };
