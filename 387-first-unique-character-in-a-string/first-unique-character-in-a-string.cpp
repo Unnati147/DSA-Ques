498d58c3666903e9d6d702bdeb69de565c674652
Unnati Mittal <153089498+Unnati147@users.noreply.github.com>
2026-01-23 23:51:21 +0530

    Time: 4 ms (72.74%) | Memory: 14.5 MB (96.17%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-
-        vector<int> freq(26,0);
+        
+    // har character (a–z) ki frequency store karne ke liye vector
+    vector<int> freq(26,0);
+    // pehla loop: string ke har character ka count nikal rahe hain
      for(int i =0 ; i<s.size();i++){
         freq[s[i] - 'a' ]++;
      }
+    //  dusra loop: string ke order me pehla unique character dhundh rahe hain
      for(int i =0; i<s.size(); i++){
         if(freq[s[i] -'a'] == 1)
             return i;
