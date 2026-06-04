05f6b9deefa586908c30580879aa3be859ec845b
Unnati Mittal <153089498+Unnati147@users.noreply.github.com>
2026-06-04 21:34:05 +0530

    Time: 13 ms (41.43%) | Memory: 31.7 MB (35.11%) - LeetSync

---
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+    //    vector<int> ans;
+    //    unordered_set <int> s;
+    //    int actualSum=0, expSum = 0;
+    //    int a,b;
+    //    int n= grid.size();
+    //    for(int i = 0 ; i<n ; i++){
+    //     for(int j =0 ; j<n; j++){
+    //         actualSum+= grid[i][j];
+    //         if(s.find(grid[i][j] )!= s.end()){
+    //             a = grid[i][j];
+    //             ans.push_back(a);
+    //         }else{
+    //             s.insert(grid[i][j]);
+    //         }
+    //     }
+    //    }
+    //    expSum= (n*n)*(n*n+1)/2;
+    //    b= expSum + a - actualSum;
+    //    ans.push_back(b);
+    //    return ans;
+
+
+
+
+
+
+
+
+
+    int n= grid.size();
+    vector<int> ans;
+    int a= 0;
+    int actualSum=0;
+    unordered_set<int> s;
+    for(int i =0; i < n; i++){
+        for(int j =0; j<n;j++){
+             actualSum += grid[i][j];
+            if(s.find(grid[i][j]) != s.end()){
+                a = grid[i][j];
+                ans.push_back(a);
+            }
+            else{
+                s.insert(grid[i][j]);
+            }
+        }
+    }
+    int totalSum= ((n*n)*(n*n+1))/2;
+    int b = totalSum + a - actualSum;
+    ans.push_back(b);
+    return ans;
+    }
+};
