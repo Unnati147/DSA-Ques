8c13abff2f6ed77ed7199d2e6df8ebd8166c75b1
Unnati Mittal <153089498+Unnati147@users.noreply.github.com>
2026-06-04 20:01:36 +0530

    Time: 0 ms (100.00%) | Memory: 65.1 MB (69.47%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        // unordered_set <int> s;
+        // int n = nums.size();
+        // for(int i =0 ; i< n ; i++){
+        //         if(s.find(nums[i]) != s.end()){
+        //            return nums[i];
+        //         }
+        //         else{
+        //             s.insert(nums[i]);
+        //         }
+        // }
+        // return -1;
+
+        // while(nums[0] != nums[nums[0]]){
+        //     swap(nums[0], nums[nums[0]]);
+        // }
+        // return nums[0];
+
+
+
+
+
+
+
+
+
+
+
+
+
+while(nums[0] != nums[nums[0]]){
+    swap(nums[0], nums[nums[0]]);
+}
+return nums[0];
+
+    }
+
+};
