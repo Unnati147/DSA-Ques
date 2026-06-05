7a217319b01655b760229ff68ec279ef052b7662
Unnati Mittal <153089498+Unnati147@users.noreply.github.com>
2026-06-05 22:52:35 +0530

    Time: 7 ms (10.11%) | Memory: 28 MB (86.89%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        // int freq = 0;   // current element ki frequency count karne ke liye
+        // int ans = 0;    // potential majority element ko ans me store krenge
+
+        // for (int i = 0; i < nums.size(); i++) {
+            
+        //     // agar frequency zero hai to naye candidate ko choose kar lo
+        //     if (freq == 0) {
+        //         ans = nums[i];
+        //     }
+
+        //     // agar current element candidate ke barabar hai
+        //     if (ans == nums[i]) {
+        //         freq++;   // frequency badha do
+        //     }
+        //     else {
+        //         freq--;   // warna frequency ghatado
+        //     }
+        // }
+
+        // // loop ke baad 'ans' majority element hoga
+        // return ans;
+
+         sort (nums.begin(), nums.end());
+        return nums[(0+nums.size()-1)/2];
+    }
+};
