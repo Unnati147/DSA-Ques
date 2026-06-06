4dfda3e9878a5c7854ae15f834ab70a751e51c70
Unnati Mittal <153089498+Unnati147@users.noreply.github.com>
2026-06-06 23:21:59 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (93.13%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,35 @@
+// class Solution {
+// public:
+// long long fact(int n ){
+    
+//  long long f = 1;
+
+//     for (int i = 1; i <= n; i++) {
+//         f *= i;
+//     }
+//     return f;
+// }
+
+//     int uniquePaths(int m, int n) {
+//       int N= (m-1) + (n-1);
+//       int r = m-1;
+
+//       return fact(N)/ (fact(r) * fact(N-r));
+//     }
+// };
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int N = m + n - 2;
+        int r = min(m - 1, n - 1);
+
+        long long ans = 1;
+
+        for (int i = 1; i <= r; i++) {
+            ans = ans * (N - r + i) / i;
+        }
+
+        return ans;
+    }
+};
