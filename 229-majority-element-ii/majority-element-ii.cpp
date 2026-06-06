427f8f01af86b3a54cd7708538a941dd3761f143
Unnati Mittal <153089498+Unnati147@users.noreply.github.com>
2026-06-07 00:38:01 +0530

    Time: 0 ms (100.00%) | Memory: 23.4 MB (96.71%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,15 +1,40 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        unordered_set<int> s;
-        for( int i = 0; i< nums.size(); i++){
-            mp[nums[i]]++;
+      int cnt1 =0;
+      int cnt2=0;
+      int maj1 =0;
+      int maj2 = 0;
 
-            if(mp[nums[i]] >nums.size()/3){
-               s.insert(nums[i]);
-            }
+      for(int i =0 ; i<nums.size(); i++){
+        if(nums[i] == maj1){
+            cnt1++;
+        }else if (nums[i] == maj2){
+            cnt2++;
+        }else if(cnt1 ==0){
+           maj1 = nums[i];
+            cnt1 =1;
+        }else if(cnt2 == 0){
+             maj2= nums[i];
+            cnt2 =1;
+        }else {
+            cnt1--;
+            cnt2--;
+
         }
-        return  vector<int>(s.begin(), s.end());
+
+      }
+      vector<int> ans;
+      int freq1=0;
+      int freq2=0;
+      int n = nums.size();
+
+      for(int num: nums){
+        if (num == maj1) freq1++;
+        else if(num == maj2) freq2++;
+      }
+      if (freq1 > n / 3) ans.push_back(maj1);
+        if (freq2 > n / 3) ans.push_back(maj2);
+        return ans;
     }
 };
