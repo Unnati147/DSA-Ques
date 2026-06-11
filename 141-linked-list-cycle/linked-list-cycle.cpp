db4ec229265352023c672d3e52f66196426e11e2
Unnati Mittal <153089498+Unnati147@users.noreply.github.com>
2026-06-11 22:25:07 +0530

    Time: 12 ms (43.77%) | Memory: 11.9 MB (22.17%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+
+bool hasCycle(ListNode *head) {
+    ListNode* slow= head;
+    ListNode* fast= head;
+    while(fast != NULL){
+        fast = fast->next;
+        if(fast != NULL){
+            fast=fast->next;
+            slow= slow->next;
+        }
+            if(slow == fast){
+            return true;
+        }
+    }
+    return false;
+
+}
+
+
+
+
+
+
+
+
+
+    // bool hasCycle(ListNode *head) {
+  
+// map wali approach
+
+    // map<ListNode*, bool> table;
+    // ListNode* temp = head;
+    // while(temp != NULL){
+    //     if(table[temp] == false){
+    //         table[temp] = true;
+    //     }else{
+    //         return true;
+    //     }
+    //     temp = temp ->next;
+    // }
+    // return false;
+// }
+};
